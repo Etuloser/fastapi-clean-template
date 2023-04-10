b094efc7e393c9e7e81c2a96470fe1dc740d5e4a
Eles <1494136313@qq.com>
2023-04-10 23:46:02 +0800

    chore: optimize user model

--- {{cookiecutter.project_slug}}/internal/entity/user.py
+++ {{cookiecutter.project_slug}}/internal/entity/user.py
@@ -1,7 +1,11 @@
-from typing import Optional
+from datetime import datetime
 
 from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
-    id: Optional[str] = Field(default=None, primary_key=True)
+    id: int = Field(primary_key=True, nullable=False)
+    name: str = Field(nullable=False)
+    created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
+    updated_at: datetime = Field(
+        default_factory=datetime.utcnow, nullable=False)
